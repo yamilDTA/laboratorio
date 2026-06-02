d769576679853fde4c0d5c093c78643182bf0709
yamilDTA <yamilticona111@gmail.com>
2026-06-02 10:21:59 -0500

    Add files via upload

---
+++ reto2.c
@@ -0,0 +1,49 @@
+#include"turtlec.h"
+
+void fractalTree( Turtle *turtle, float length , int depth ) {
+  if( depth == 0 || length < 5)
+    return ;
+  
+  if( depth > 5){
+	  turtleSetColor ( turtle , 120 , 70 , 20) ; //marron
+  }else if (depth >2){
+	  turtleSetColor ( turtle , 100 ,255, 100);  //verde mas claro
+  }else{
+	  turtleSetColor ( turtle , 0 , 200 , 0) ;  //verde
+  }
+  
+  turtleForward ( turtle , length ) ;
+
+  turtleLeft ( turtle , 45) ;
+  fractalTree ( turtle , length * 0.7 , depth - 1) ;
+
+  turtleRight ( turtle , 90) ;
+  fractalTree ( turtle , length * 0.4 , depth - 1) ;
+  
+  turtleLeft( turtle, 45);
+
+turtlePenUp(turtle);
+turtleBackward ( turtle , length ) ;
+turtlePenDown (turtle);
+	
+}
+
+
+int main(void){
+  TurtleApp *app=turtleAppCreate(1200,600,"test line");
+  if(app==NULL)
+    return 1;
+  Turtle *t = turtleAppGetTurtle(app);
+  turtlePenUp(t);
+  turtleGoTo(t,600.0f,550.0f);
+  turtlePenDown(t);
+  turtleSetColor(t,128,64,0);
+  turtleSetSpeed(t,8.0f);
+  turtleLeft(t,90.0f);
+  fractalTree(t , 80 ,7 );
+  turtleAppRun(app);
+  turtleAppDestroy(app);
+  return 0;
+
+}
+
