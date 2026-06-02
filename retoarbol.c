fcbbe84f6f928a85e28e7271f5011e729856dc5e
yamilDTA <yamilticona111@gmail.com>
2026-06-01 22:58:38 -0500

    Add files via upload

---
+++ retoarbol.c
@@ -0,0 +1,36 @@
+#include"turtlec.h"
+
+void fractalTree( Turtle *turtle, float length , int depth ) {
+  if( depth == 0 || length < 5)
+    return ;
+  turtleForward ( turtle , length ) ;
+
+  turtleLeft ( turtle , 30) ;
+  fractalTree ( turtle , length * 0.7 , depth - 1) ;
+
+  turtleRight ( turtle , 60) ;
+  fractalTree ( turtle , length * 0.7 , depth - 1) ;
+  
+  turtleLeft( turtle, 30);  
+  turtleBackward ( turtle , length ) ;
+  
+}
+
+
+int main(void){
+  TurtleApp *app=turtleAppCreate(1200,600,"test line");
+  if(app==NULL)
+    return 1;
+  Turtle *t = turtleAppGetTurtle(app);
+  turtlePenUp(t);
+  turtleGoTo(t,600.0f,550.0f);
+  turtlePenDown(t);
+  turtleSetColor(t,255,100,0);
+  turtleSetSpeed(t,5.0f);
+  turtleLeft(t,90.0f);
+  fractalTree(t , 100 ,7 );
+  turtleAppRun(app);
+  turtleAppDestroy(app);
+  return 0;
+
+}
