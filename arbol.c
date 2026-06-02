eda8c4793ef6d4b4d77facfae6781b9daa4d1541
yamilDTA <yamilticona111@gmail.com>
2026-06-02 10:56:46 -0500

    Update arbol.c

--- arbol.c
+++ arbol.c
@@ -18,17 +18,17 @@ void fractalTree( Turtle *turtle, float length , int depth ) {
 
 
 int main(void){
-  TurtleApp *app=turtleAppCreate(1200,600,"test line");
+  TurtleApp *app=turtleAppCreate(1000,600,"test line");
   if(app==NULL)
     return 1;
   Turtle *t = turtleAppGetTurtle(app);
   turtlePenUp(t);
-  turtleGoTo(t,600.0f,550.0f);
+  turtleGoTo(t,500.0f,550.0f);
   turtlePenDown(t);
   turtleSetColor(t,255,100,0);
   turtleSetSpeed(t,5.0f);
   turtleLeft(t,90.0f);
-  fractalTree(t , 80 ,7 );
+  fractalTree(t , 200 ,16 );
   turtleAppRun(app);
   turtleAppDestroy(app);
   return 0;
