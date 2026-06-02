105fc521f5536357fcf2b41a3f0c33fb3e056a21
yamilDTA <yamilticona111@gmail.com>
2026-06-01 23:04:05 -0500

    Update retoarbol.c

--- retoarbol.c
+++ retoarbol.c
@@ -8,9 +8,12 @@ void fractalTree( Turtle *turtle, float length , int depth ) {
   turtleLeft ( turtle , 30) ;
   fractalTree ( turtle , length * 0.7 , depth - 1) ;
 
-  turtleRight ( turtle , 60) ;
+  turtleRight ( turtle , 30) ;  //esta pone la rama del medio como antes moviamos defrente 60 a la derecha 
   fractalTree ( turtle , length * 0.7 , depth - 1) ;
   
+  turtleRight ( turtle , 30) ;
+  fractalTree ( turtle , length * 0.7 , depth - 1) ;
+
   turtleLeft( turtle, 30);  
   turtleBackward ( turtle , length ) ;
   
