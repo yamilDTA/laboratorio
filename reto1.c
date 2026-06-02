61a4336bc69bce0c0ef634690ff9f25461841b3f
yamilDTA <yamilticona111@gmail.com>
2026-06-02 11:25:19 -0500

    Update reto1.c

--- reto1.c
+++ reto1.c
@@ -5,33 +5,33 @@ void fractalTree( Turtle *turtle, float length , int depth ) {
     return ;
   turtleForward ( turtle , length ) ;
 
-  turtleLeft ( turtle , 55) ;
+  turtleLeft ( turtle , 45) ;
   fractalTree ( turtle , length * 0.5 , depth - 1) ;
 
-  turtleRight ( turtle , 55) ;  //esta pone la rama del medio como antes moviamos defrente 55 a la izquierda esta la regresa al centro  
+  turtleRight ( turtle , 45) ;  //esta pone la rama del medio como antes moviamos defrente 55 a la izquierda esta la regresa al centro  
   fractalTree ( turtle , length * 0.5 , depth - 1) ;
   
-  turtleRight ( turtle , 55) ;
+  turtleRight ( turtle , 45) ;
   fractalTree ( turtle , length * 0.5 , depth - 1) ;
 
-  turtleLeft( turtle, 55);  
+  turtleLeft( turtle, 45);  
   turtleBackward ( turtle , length ) ;
   
 }
 
 
 int main(void){
-  TurtleApp *app=turtleAppCreate(1200,600,"test line");
+  TurtleApp *app=turtleAppCreate(100,600,"test line");
   if(app==NULL)
     return 1;
   Turtle *t = turtleAppGetTurtle(app);
   turtlePenUp(t);
-  turtleGoTo(t,600.0f,550.0f);
+  turtleGoTo(t,500.0f,550.0f);
   turtlePenDown(t);
   turtleSetColor(t,255,100,0);
   turtleSetSpeed(t,5.0f);
   turtleLeft(t,90.0f);
-  fractalTree(t , 100 ,4 );
+  fractalTree(t , 200 ,5 );
   turtleAppRun(app);
   turtleAppDestroy(app);
   return 0;
